a4e39cf5b70e106734b03a9badbb7e1a112d55e9
Ios Kateyka <ioskateyka@gmail.com>
2021-06-12 17:23:19 +0300

    Initial Commit

---
+++ ContactListTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactListTableView
+//
+//  Created by Екатерина Боровкова on 12.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
